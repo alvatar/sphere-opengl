0bb0bbb551c2812da2586758b232e1b78ffd5175
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-10-15 21:19:49 +0200

    add dependencies in config.scm

--- config.scm
+++ config.scm
@@ -1,1 +1,21 @@
 (sphere: "opengl")
+(dependencies:
+ (gl-es (include
+         (base: ffi#))
+        (load
+         (base: ffi)))
+ ((opengl: gl-es version: (debug))
+  (include
+   (base: ffi# version: (debug)))
+  (load
+   (base: ffi version: (debug))))
+ (gl (include
+      (base: ffi#))
+     (load
+      (base: ffi)))
+ ((opengl: gl version: (debug))
+  (include
+   (base: ffi# version: (debug)))
+  (load
+   (base: ffi version: (debug)))))
+
