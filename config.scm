129218862e1e8671f4b097f120aae0bf29604ed4
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-11-14 15:38:11 +0100

    adapted to separation between headers and macro files

--- config.scm
+++ config.scm
@@ -1,21 +1,21 @@
 (sphere: "opengl")
 (dependencies:
  (gl-es (include
-         (base: ffi#))
+         (base: ffi-macros))
         (load
          (base: ffi)))
  ((= gl-es version: (debug))
   (include
-   (base: ffi# version: (debug)))
+   (base: ffi-macros version: (debug)))
   (load
    (base: ffi version: (debug))))
  (gl (include
-      (base: ffi#))
+      (base: ffi-macros))
      (load
       (base: ffi)))
  ((= gl version: (debug))
   (include
-   (base: ffi# version: (debug)))
+   (base: ffi-macros version: (debug)))
   (load
    (base: ffi version: (debug)))))
 
